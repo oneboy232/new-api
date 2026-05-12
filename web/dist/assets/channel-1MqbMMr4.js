0c08e213f875ea3f029e3ec6c5c6b659581b9dcb
wangbo <wabo.wangbo@gmail.com>
2026-05-12 23:41:50 +0800

    移除模式选项

--- web/dist/assets/channel-1MqbMMr4.js
+++
@@ -1,1 +0,0 @@
-import{as as o,at as s}from"./index-B2s4bIG3.js";const t=(a,r)=>o.lang.round(s.parse(a)[r]);export{t as c};
--- web/dist/assets/clone-DivqUNMi.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./_baseUniq-CRtKHzBz.js";var e=4;function a(o){return r(o,e)}export{a as c};
